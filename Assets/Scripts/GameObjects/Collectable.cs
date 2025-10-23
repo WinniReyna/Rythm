16129646c2c05434cdf284a42c096c2ef13402bb
Von Valentine <winona.reyna@uniat.edu.mx>
2025-10-23 10:11:25 -0600

    adding tooltips and templates

--- Assets/Scripts/GameObjects/Collectable.cs
+++ Assets/Scripts/GameObjects/Collectable.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour, IInteractable
 {
+    [Tooltip("Coloca aquí el ItemSO de scriptableObject que creaste para este item.")]
     [SerializeField] private ItemSO itemData;
+    [Tooltip("Cantidad de objeto recolectado, puede empezar x1 o x5 o n cantidad de cantidad de item recolectado.")]
     [SerializeField] private int quantity = 1;
 
     public void Interact()
--- Assets/Scripts/ScriptableObject/ItemSO.cs
+++ Assets/Scripts/ScriptableObject/ItemSO.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 public class ItemSO : ScriptableObject
 {
     [Header("Identificador único")]
+    [Tooltip("Puede ser el nombre del item.")]
     public string itemID;
 
     [Header("Datos visuales")]
+    [Tooltip("Nombre que aparecerá en la UI.")]
     public string itemName;
+    [Tooltip("Descripción del item.")]
     [TextArea] public string description;
+    [Tooltip("Imagen del item.")]
     public Texture2D icon;
 
     [Header("Instancia para tirar")]
+    [Tooltip("El ítem en escena debe ser un prefab para poder instanciarlo nuevamente al sacarlo del inventario.")]
     public GameObject prefab;
 
     [Header("Opcional")]
+    [Tooltip("Si quieren que tenga un sonido el objeto al recoger. Por el momento puede estar vacío.")]
     public AudioClip pickupSound;
 }
