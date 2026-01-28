d376434fb338fbd82e0f1dc744cc439a8df5e63a
Von Valentine <winona.reyna@uniat.edu.mx>
2026-01-28 14:10:57 -0600

    prefab Player added

--- Assets/Scripts/Animations/PlayerAnimator.cs
+++ Assets/Scripts/Animations/PlayerAnimator.cs
@@ -2,9 +2,7 @@
 
 public class PlayerAnimator : MonoBehaviour, IPlayerAnimator
 {
-    private Animator animator;
-
-    void Awake() => animator = GetComponent<Animator>();
+    [SerializeField] private Animator animator;    
 
     public void SetSpeed(float speed) => animator.SetFloat("Speed", speed);
     public void SetRunning(bool isRunning) => animator.SetBool("IsRunning", isRunning);
