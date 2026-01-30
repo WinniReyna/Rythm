8f9aab5a6266245adb13f121e8f660bef2b0ff06
Von Valentine <winona.reyna@uniat.edu.mx>
2026-01-30 08:58:48 -0600

    fixing key

--- Assets/Scripts/Animations/PlayerAnimator.cs
+++ Assets/Scripts/Animations/PlayerAnimator.cs
@@ -2,7 +2,9 @@
 
 public class PlayerAnimator : MonoBehaviour, IPlayerAnimator
 {
-    [SerializeField] private Animator animator;    
+    private Animator animator;
+
+    void Awake() => animator = GetComponent<Animator>();
 
     public void SetSpeed(float speed) => animator.SetFloat("Speed", speed);
     public void SetRunning(bool isRunning) => animator.SetBool("IsRunning", isRunning);
